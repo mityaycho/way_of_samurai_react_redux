d2c0a87b16362cb17bbcf434c5b65f51c726ffd2
mityaycho <mityaycho@gmail.com>
2019-10-08 13:53:00 +0300

    first steps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+ const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://about.canva.com/wp-content/uploads/sites/3/2016/08/logos-1.png" alt=""/>
       </header>
+      <nav className="nav">
+        <div>
+          <a href="">Profile</a>
+        </div>
+        <div>
+          <a href="">Messages</a>
+        </div>
+        <div>
+          <a href="">News</a>
+        </div>
+        <div>
+          <a href="">Music</a>
+        </div>
+        <div>
+          <a href="">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="http://h2.hqtexture.com/98/9735/1457619934-1.jpg" alt=""/>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>post1</div>
+            <div>post2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
