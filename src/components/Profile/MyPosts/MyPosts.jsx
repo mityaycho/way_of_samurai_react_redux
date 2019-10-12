285f8b583608c3a8ecc05c4f5610c151fd6bf909
mityaycho <mityaycho@gmail.com>
2019-10-12 21:47:16 +0300

    add onClick test

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,16 +5,21 @@ import Post from "./Post";
 const MyPosts = (props) => {
 
   let postsElements = props.posts.map(el => <Post message={el.message} likesCount={el.likesCount} />)
+  let newPostElement = React.createRef();
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert(text);
+  };
 
   return (
     <div className={style.postBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea name="" id="" cols="30" rows="1"></textarea>
+          <textarea ref={newPostElement} name="" id="" cols="30" rows="1"></textarea>
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       <div className={style.post}>
