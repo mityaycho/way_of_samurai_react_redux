e9e3ea159900186cb9d6aafb9c34a11242af6134
mityaycho <mityaycho@gmail.com>
2019-10-12 21:38:07 +0300

    moving BrowserRouter in index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import {Route} from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
@@ -7,20 +7,17 @@ import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 
 
-
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <div className="app-wrapper">
-        <Header />
-        <NavBar />
-        <div className="app-wrapper-content">
-          <Route path="/dialogs" render={ () => <Dialogs state={props.state.dialogsPage} /> } />
-          <Route path="/profile" render={ () => <Profile state={props.state.profilePage} /> } />
+    <div className="app-wrapper">
+      <Header/>
+      <NavBar/>
+      <div className="app-wrapper-content">
+        <Route path="/dialogs" render={() => <Dialogs state={props.state.dialogsPage}/>}/>
+        <Route path="/profile" render={() => <Profile state={props.state.profilePage}/>}/>
 
-        </div>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
