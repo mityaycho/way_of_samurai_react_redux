050aa0a0f2b2b9f6804f435c4013bc7e22b21fc5
mityaycho <mityaycho@gmail.com>
2019-10-20 19:28:30 +0300

    install redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from './redux/state'
+import store from './redux/store'
 import {BrowserRouter} from "react-router-dom";
 
 let rerenderEntireTree = (state) => {
@@ -17,7 +17,8 @@ let rerenderEntireTree = (state) => {
 }
 
 rerenderEntireTree(store.getState());
-store.subscribe(rerenderEntireTree)
+
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
