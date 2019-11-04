74b2a450fa7df833ab34c8206b7a9255732d0bd5
mityaycho <mityaycho@gmail.com>
2019-11-05 00:18:43 +0300

    add login user

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import {Route} from "react-router-dom";
 import './App.css';
-import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
-import Profile from './components/Profile/Profile';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <Header/>
-      <NavBar/>
+      <HeaderContainer />
+      <NavBar />
       <div className="app-wrapper-content">
         <Route path="/dialogs" render={() => <DialogsContainer />} />
-        <Route path="/profile" render={() => <ProfileContainer />}/>
+        <Route path="/profile/:userId?" render={() => <ProfileContainer />}/>
         <Route path="/users" render={() => <UsersContainer />}/>
       </div>
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./Header.module.css";
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={styles.header}>
       <img src="https://thequietsite.co.uk/wp-content/uploads/2019/05/tqs_logo_WhtGreen-1.png" alt=""/>
+      <div className={styles.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+      </div>
     </header>
   );
 }
