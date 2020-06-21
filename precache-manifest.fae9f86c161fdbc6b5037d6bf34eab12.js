c6c2b169dead1879fdb0cf2a72abea9a5de5aa85
mityaycho <mityaycho@gmail.com>
2020-06-21 22:46:15 +0300

    Updates

---
+++ precache-manifest.fae9f86c161fdbc6b5037d6bf34eab12.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87a2f29beaccbb21618b06717e5f687",
+    "url": "/mityaycho/way_of_samurai_react_redux/index.html"
+  },
+  {
+    "revision": "7a299c7d5356e100fbca",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/css/3.7f8ff71f.chunk.css"
+  },
+  {
+    "revision": "09d9c61e4dc7a0a2d83c",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/css/4.5512173f.chunk.css"
+  },
+  {
+    "revision": "4aa30fdd3de38c6ebee4",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/css/main.9860b01e.chunk.css"
+  },
+  {
+    "revision": "1676c763474a5bd990f9",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/js/2.46c0e033.chunk.js"
+  },
+  {
+    "revision": "7a299c7d5356e100fbca",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/js/3.296fc256.chunk.js"
+  },
+  {
+    "revision": "09d9c61e4dc7a0a2d83c",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/js/4.f92513f1.chunk.js"
+  },
+  {
+    "revision": "4aa30fdd3de38c6ebee4",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/js/main.87ee2dea.chunk.js"
+  },
+  {
+    "revision": "2f7339fc56eb02213e93",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/js/runtime-main.4c7d706f.js"
+  },
+  {
+    "revision": "bb68a89d59ffef59443609ec11105d36",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/media/Bigstock_349790512.bb68a89d.jpg"
+  },
+  {
+    "revision": "408292d8c1a25d6f80734828dec198dd",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/media/aug-4-2017-6-11-pm.408292d8.gif"
+  },
+  {
+    "revision": "5d432515cba316c421fc0b214b630a02",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/media/roller.5d432515.jpg"
+  },
+  {
+    "revision": "ce1bd8f5f55e751129b95494d58fe313",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/media/star-space-tile.ce1bd8f5.jpg"
+  },
+  {
+    "revision": "b6e9713ac7b371a87441d34a1a900712",
+    "url": "/mityaycho/way_of_samurai_react_redux/static/media/user-icon-small-size.b6e9713a.png"
+  }
+]);
