bbfe4449f51571caece393839c260fbd6bd0167d
mityaycho <mityaycho@gmail.com>
2020-06-23 20:02:20 +0300

    deleta hash-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React, {Component} from "react";
-import {Redirect, Route, Switch, withRouter, HashRouter} from "react-router-dom";
+import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import UsersContainer from "./components/Users/UsersContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
-import {connect, Provider} from "react-redux";
-import {compose} from "redux";
-import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import store from "./redux/redux-store";
-import {withSuspense} from "./hoc/withSuspense";
+
+import { BrowserRouter, Redirect, Route, Switch, withRouter } from "react-router-dom";
+import { connect, Provider } from "react-redux";
+import { compose } from "redux";
+import { initializeApp } from "./redux/app-reducer";
+import { withSuspense } from "./hoc/withSuspense";
+
 
 const DialogsContainer = React.lazy(() => import("./components/Dialogs/DialogsContainer"));
 const ProfileContainer = React.lazy(() => import("./components/Profile/ProfileContainer"));
@@ -49,11 +51,11 @@ const AppContainer = compose(
   connect(mapStateToProps, {initializeApp}))(App);
 
 const AppWithBrowserRouter = (props) => {
-  return <HashRouter>
+  return <BrowserRouter>
     <Provider store={store}>
       <AppContainer />
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 };
 
 export default AppWithBrowserRouter;
